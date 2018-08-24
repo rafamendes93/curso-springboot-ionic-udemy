83fa2e8ed4e4a54aacecedd757e67f965e87f7a3
Rafael Mendes <bk.rafa@gmail.com>
2018-08-24 14:05:01 -0300

    Personalização do retorno de erro da validação

--- src/main/java/com/rafael/cursomc/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rafael/cursomc/cursomc/dto/CategoriaDTO.java
@@ -11,7 +11,7 @@ public class CategoriaDTO implements Serializable {
 
     private Integer id;
 
-    @NotEmpty(message = "O nome não pode estar vazio")
+    @NotEmpty(message = "Por favor inserir o nome")
     @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
