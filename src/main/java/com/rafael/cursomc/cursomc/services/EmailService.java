a11a6300f6bf3ba80755430e0e5d0a4f090f380b
Rafael Mendes <bk.rafa@gmail.com>
2018-08-27 18:10:27 -0300

    Implementando envio de e-mail HTML

--- src/main/java/com/rafael/cursomc/cursomc/services/EmailService.java
+++ src/main/java/com/rafael/cursomc/cursomc/services/EmailService.java
@@ -3,9 +3,15 @@ package com.rafael.cursomc.cursomc.services;
 import com.rafael.cursomc.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido pedido);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido pedido);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
