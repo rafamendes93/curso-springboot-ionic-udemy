d1934b037cd62e0a998a8481c14bf903a846cb9e
Rafael Mendes <bk.rafa@gmail.com>
2018-08-27 16:02:54 -0300

    Implementando SMTPService com SMTP do GMAIL

--- src/main/java/com/rafael/cursomc/cursomc/services/DBService.java
+++ src/main/java/com/rafael/cursomc/cursomc/services/DBService.java
@@ -114,7 +114,7 @@ public class DBService {
         cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 
 
-        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+        Cliente cli1 = new Cliente(null, "Rafael Mendes", "bk.rafa@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
